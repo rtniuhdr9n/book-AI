fd25fd4fe7b2ad8f03073030c2e41cfbe75dbb95
rtniuhdr9n <137862620+rtniuhdr9n@users.noreply.github.com>
2026-03-20 14:44:09 +0800

    Delete .idea/Hello.java

--- .idea/Hello.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld {
-    // main方法：程序的入口（固定写法，JVM 会从这里开始执行）
-    public static void main(String[] args) {
-        // 核心语句：向控制台输出 "Hello World!"
-        System.out.println("Hello World!");
-    }
-}
